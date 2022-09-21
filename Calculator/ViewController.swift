b54f41d67e2c88f44fcb89daf1412d922999b356
adry9204 <adriana920420@gmail.com>
2022-09-20 22:05:39 -0400

    I added a new label for the calculator operation's history.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CalculationLabel: UILabel!
     @IBOutlet weak var ResultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
