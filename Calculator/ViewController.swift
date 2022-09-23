2b2ba33696ab314c2eb9685838c8deef91b9061f
adry9204 <adriana920420@gmail.com>
2022-09-23 11:34:57 -0400

    Internal Documentation complete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -3,12 +3,13 @@
 //  Calculator
 //
 /* Created and Developed by
-    Aurela Bala -
+    Aurela Bala - 301279255
     Adriana Diaz Torres - 301157161
-    Manmeen Kaur -
+    Manmeen Kaur - 301259638
     
     Date Created: 20/09/2022
     Simple Calculator App. It will implement all basic mathematical operations, as well as a Clear and Backspace button in portrait mode. In landscape mode, it will feature a scientific calculator. It has a custom design.
+    Verion: 1.0.0
  
  */
 
@@ -18,15 +19,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Label outlets
     @IBOutlet weak var CalculationLabel: UILabel!
     @IBOutlet weak var ResultLabel: UILabel!
     
     
+    //App's Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    
+    
+    //event handlers for when a button is pressed
+    @IBAction func EraseBttnPressed(_ sender: UIButton) {}
+    
+    @IBAction func OperatorBttnPressed(_ sender: UIButton) {}
+    
+    @IBAction func NumberBttnPressed(_ sender: UIButton) {}
+    
+    
+    
 }
 
