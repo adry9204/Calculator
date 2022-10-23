06524951ffc21a7424a0778af0a0e87bd0f8a349
AurelaBala <aurelatosuni2022@gmail.com>
2022-10-23 03:45:04 -0400

    creating new methods  for ( rand, tang, tan, sin, sinh, cos, cosh )

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,11 +7,12 @@
  Adriana Diaz Torres - 301157161
  Manmeen Kaur - 301259638
  
- Date Created: 5/10/2022
- Simple Calculator App. It performs all basic operation in accordance with the interface we implemented in version 1.0.0.
-    Addition, Subtraccion, Multiplication, Divition, Percentage and +/-, Clear and Backspace
+ Date Created: 19/10/2022
+ Advanced Calculator App. It performs all basic operation in accordance with the interface we implemented in version 1.0.0.
+ It performs 30 new operations only on landscape mode.
+    Addition, Subtraccion, Multiplication, Divition, Percentage and +/-, Clear, Backspace and 30 new operations.
     It allows for combined operations, evaluating them according to the priority rules as well as several data format checks, such as preventing a number to be entered with 2 dots or a number that starts with 00000.
- Version: 1.2.0
+ Version: 1.3.0
  
  */
 
@@ -24,6 +25,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var ResultLabel: UILabel!
     @IBOutlet weak var ResultLabelLandscape: UILabel!
     @IBOutlet weak var CalculationLabelLandscape: UILabel!
+    
+    var expression = ""
+    //var lastExpression = ""
+    
+    
     
     //App's Lifecycle
     override func viewDidLoad()
@@ -49,20 +55,27 @@ class ViewController: UIViewController {
         switch (buttonText) {
         case "C":
             clearAll()
+            expression = ""
             //backspace case
         default:
             if(CalculationLabel.text!.count == 1)
             {
                 CalculationLabel.text = "0"
                 CalculationLabelLandscape.text = "0"
+                expression = ""
+                
             }
             
             else
             {
                 CalculationLabel.text?.removeLast()
                 CalculationLabelLandscape.text?.removeLast()
+                //expression.removeAll(lastExpressi)
+
             }
         }
+        
+        print(expression)
     }
     
     
@@ -89,6 +102,7 @@ class ViewController: UIViewController {
             
             if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷") {
                 CalculationLabel.text?.removeLast()
+                expression.removeLast()
             }
             //allow to exchange one unary operator for other
             //avoid the case of unary operator behind binary operator
@@ -99,11 +113,31 @@ class ViewController: UIViewController {
             if (lastCharacter == "±" || lastCharacter == "%"){
                 CalculationLabel.text?.removeLast()
             }
+            
         }
+        
+        
+//        let numbers = Int(lastCharacter!)
+//        if(buttonText == "%") {
+//
+//            if((numbers != nil) == true) {
+//                //CalculationLabel.text?.append(buttonText!)
+//                CalculationLabel.text?.removeLast()
+//                CalculationLabelLandscape.text = CalculationLabel.text
+//                //expression.removeLast()
+//            }
+//
+//            else {
+//                CalculationLabel.text?.removeLast()
+//            }
+//            
+//        }
         
         //add new character to calculation label
         CalculationLabel.text?.append(buttonText!)
         CalculationLabelLandscape.text = CalculationLabel.text
+        expression.append(buttonText!)
+        print(expression)
         
         
         
@@ -115,7 +149,7 @@ class ViewController: UIViewController {
     @IBAction func EqualBttnPressed(_ sender: UIButton) {
         var expression = CalculationLabel.text!
         
-        //an expression that start with a negative number should be evaluated as 0-expressio
+        //an expression that start with a negative number should be evaluated as 0-expression
         if(CalculationLabel.text!.first == "-"){
             expression = "0" + expression
         }
@@ -165,14 +199,19 @@ class ViewController: UIViewController {
                 
                 CalculationLabelLandscape.text?.removeLast()
                 CalculationLabelLandscape.text?.append(buttonText!)
+                expression = CalculationLabel.text!
+                print(expression)
                 return
             }
             
             CalculationLabel.text?.append(buttonText!)
             CalculationLabelLandscape.text?.append(buttonText!)
+            expression.append(buttonText!)
+            
             
 
         }
+        print(expression)
         
     }
     
@@ -195,4 +234,273 @@ class ViewController: UIViewController {
     }
     
     
+    
+    //Event handlers when π button is pressed
+
+    
+    @IBAction func piButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+        //expression = (CalculationLabelLandscape.text!)
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "π"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression += "p"
+            //print(expression)
+            
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            //lastExpression = "pi"
+            expression += "p"
+            CalculationLabelLandscape.text?.append(buttonText!)
+            CalculationLabel.text = CalculationLabelLandscape.text
+            
+            //print(lastExpression)
+           
+        }
+        print(expression)
+            
+    }
+    
+    //Event handlers when Rand button is pressed
+    
+    @IBAction func randButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+        //let newNumber = ExpressionEvaluator.Evaluate(expression: "R")
+        var newNumber = drand48()
+        let newNumberString = String(newNumber)
+        
+        
+        //expression = (CalculationLabelLandscape.text!)
+        if(CalculationLabelLandscape.text == "0")
+        {
+            CalculationLabelLandscape.text = newNumberString
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression.append(newNumberString)
+            
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±")
+        {
+            
+            expression += newNumberString
+            CalculationLabelLandscape.text?.append(newNumberString)
+            CalculationLabel.text = CalculationLabelLandscape.text
+            
+            
+            
+        }
+        
+        print(expression)
+    }
+    
+    //Event handlers when tanh button is pressed
+    
+    @IBAction func tanhButtonPressed(_ sender: UIButton) {
+        
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        let numbers = Int(lastCharacter!)
+        //let lastCharacterString = String(lastCharacter)
+        if ((numbers != nil) == true || lastCharacter == ")" )  {
+           expression.append("o")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+    }
+    
+    //Event handlers when cosh button is pressed
+
+    
+    @IBAction func coshButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        if let numbers = (Int(lastCharacter!))  {
+           expression.append("k")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+        
+    }
+    
+    //Event handlers when sin button is pressed
+    
+    @IBAction func sinhButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        if let numbers = (Int(lastCharacter!))  {
+           expression.append("j")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+        
+    }
+    
+    //Event handlers when sin button is pressed
+
+    
+    @IBAction func tanButtonPressed(_ sender: UIButton) {
+        
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        if let numbers = (Int(lastCharacter!))  {
+           expression.append("i")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+    }
+    
+    //Event handlers when cos button is pressed
+    
+    
+    @IBAction func cosButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        if let numbers = (Int(lastCharacter!))  {
+           expression.append("h")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+    }
+    
+    //Event handlers when sin button is pressed
+    
+    @IBAction func sinButtonPressed(_ sender: UIButton) {
+        let button = sender as UIButton
+        let buttonText = button.titleLabel?.text
+        let lastCharacter = CalculationLabelLandscape.text?.last.map(String.init)
+        
+       
+        if(CalculationLabelLandscape.text == "0") {
+            CalculationLabelLandscape.text = "0"
+            CalculationLabel.text = CalculationLabelLandscape.text
+            expression = ""
+           
+        }
+        
+        if(lastCharacter == "+" || lastCharacter == "-" || lastCharacter == "x" || lastCharacter == "÷"  || lastCharacter == "±") {
+            
+            return
+            
+        }
+        
+        if let numbers = (Int(lastCharacter!))  {
+           expression.append("g")
+           CalculationLabelLandscape.text?.append(buttonText!)
+           CalculationLabel.text = CalculationLabelLandscape.text
+  }
+    
+        print(expression)
+    }
+    
+    //Event handlers when x! button is pressed
+    
+    @IBAction func factorialButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
 }
+
+
+
